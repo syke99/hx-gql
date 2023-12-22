28ed56b3262a519525c86071d925206e0fc9accc
syke99 <quinnmillican@gmail.com>
2023-12-22 12:19:17 -0700

    working gql server example

---
+++ cmd/graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+//go:generate go run github.com/99designs/gqlgen generate
+
+import "github.com/syke99/example-gql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	Responses map[string]*model.Response
+}
